b67a9be42828630fb958eee70b4d3c74407bc587
Tolu Adesina <tadesina90@gmail.com>
2019-05-06 11:53:27 +0100

    Function to convert a string, sentence or word to uppercase

---
+++ toUpper/toUpper.js
@@ -0,0 +1,9 @@
+String.prototype.toUpper = function() {
+  var newString = '';
+  for (var str in this) {
+    code = this.charCodeAt(str)
+    if (code >= 97 && code <= 122) code -= 32;
+    newString += String.fromCharCode(code);
+  }
+  return newString;
+}
