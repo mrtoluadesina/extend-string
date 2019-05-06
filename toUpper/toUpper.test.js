bc5d5f1e23747cace80c42e7b3348c1e5081cca9
Tolu Adesina <tadesina90@gmail.com>
2019-05-06 11:17:14 +0100

    Test Case Updated - added comment to help a reader know the test case is using a regex pattern to match the output string

--- toUpper/toUpper.test.js
+++ toUpper/toUpper.test.js
@@ -2,5 +2,5 @@ var toUpper = require('./toUpper');
 
 test('Check to know if the string is converted properly to capital letters', function() {
   var aString = 'i am a string';
-  expect(aString.toUpper()).toMatch(/[A-Z\s]{13}/);
+  expect(aString.toUpper()).toMatch(/[A-Z\s]{13}/); // using regex as the matcher for the function
 });
