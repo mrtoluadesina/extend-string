233fa9c2a43508b24a58b66d351e752649ebce09
Tolu Adesina <tadesina90@gmail.com>
2019-05-06 18:25:00 +0100

    Alternating Case function created - the string alternates and passes the test

--- alternatingCase/alternatingCase.js
+++ alternatingCase/alternatingCase.js
@@ -1,5 +1,12 @@
-String.prototype.alternatingCase = function() {
+var toUpper = require('../toUpper/toUpper');
+var toLower = require('../toLower/toLower');
 
+String.prototype.alternatingCase = function() {
+  var newString = '';
+  for (var i = 0; i < this.length; i++) {
+    (i % 2 === 0) ? newString += this[i].toLower() : newString += this[i].toUpper();
+  }
+  return newString;
 }
 
 module.exports = String.prototype.alternatingCase;
