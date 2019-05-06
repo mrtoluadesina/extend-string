2e4bb512678baa0fc9c401b2d48b3fcafef7cbf5
Tolu Adesina <tadesina90@gmail.com>
2019-05-06 10:30:24 +0100

    Updated the import path for the testing

--- index.spec.js
+++ index.spec.js
@@ -1,4 +1,4 @@
-var AString = require('.');
+var index = require('./index');
 
 test('Check to see that a string has at least one vowel and returns true, else returns false', function() {
   expect()
