ff792c287269a8644ee019ad23c08c40140fa666
Tolu Adesina <tadesina90@gmail.com>
2019-05-06 17:18:26 +0100

    Updated the inverseCase method, refactored it and made use of the ternary operator (in place of if)

--- inverseCase/inverseCase.js
+++ inverseCase/inverseCase.js
@@ -4,11 +4,7 @@ var toLower = require('../toLower/toLower');
 String.prototype.inverseCase = function() {
   var newString = '', regex = /[a-z]/;
   for (var i = 0; i < this.length; i++) {
-    if (regex.test(this[i])) {
-      newString += this[i].toUpper();
-    } else {
-      newString += this[i].toLower();
-    }
+    regex.test(this[i]) ? newString += this[i].toUpper() : newString += this[i].toLower();
   }
   return newString;
 }
