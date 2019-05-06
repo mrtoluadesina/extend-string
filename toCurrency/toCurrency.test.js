125da0cb93d57ab0d74fba4ed43d1595d63b50ae
Tolu Adesina <tadesina90@gmail.com>
2019-05-06 14:42:30 +0100

    Test Case for checking if the string entered is returned as a string with the right commas, and also checks for when it is not a number in the string

--- toCurrency/toCurrency.test.js
+++ toCurrency/toCurrency.test.js
@@ -1,7 +1,7 @@
-var toCurrency = require('./toCurrency');
+var toCurrency = require('./toCurrency.js');
 
 test('returns a currency representation of the inputed string', function() {
-  var money = 12569400, notMoney = 'threeten';
-  expect(money.toCurrency()).toBe(12,569,400);
-  expect(money.toCurrency()).toBe('Not a Currency');
+  var money = '12569400', notMoney = 'threeten';
+  expect(money.toCurrency()).toBe('12,569,400.00');
+  expect(notMoney.toCurrency()).toBe('NaN');
 });
