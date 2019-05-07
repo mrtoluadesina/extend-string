abfd0c35f02e6b124918dc59d91001ce2ee63819
Tolu Adesina <tadesina90@gmail.com>
2019-05-07 13:55:55 +0100

    Removed braces from the if statement in toUpper method, since it is a one-liner

--- toUpper/toUpper.js
+++ toUpper/toUpper.js
@@ -2,7 +2,7 @@ String.prototype.toUpper = function() {
   var newString = '';
   for (var i = 0; i < this.length; i++) {
     code = this.charCodeAt(i);
-    if (code >= 97 && code <= 122) { code -= 32; }
+    if (code >= 97 && code <= 122) code -= 32;
     newString += String.fromCharCode(code);
   }
   return newString;
