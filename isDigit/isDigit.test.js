63c3c4aa67d8269958281dba88dbed36e7ecde7c
Tolu Adesina <tadesina90@gmail.com>
2019-05-07 13:25:25 +0100

    Made declarations per line for readability

--- isDigit/isDigit.test.js
+++ isDigit/isDigit.test.js
@@ -1,7 +1,8 @@
 var isDigit = require('./isDigit');
 
 test('Checks that a number is a digit - only one number - and returns true', function() {
-  var trueDigit = '3', falseDigit = '34';
+  var trueDigit = '3', 
+      falseDigit = '34';
   expect(trueDigit.isDigit()).toBeTruthy();
   expect(falseDigit.isDigit()).toBeFalsy();
 });
