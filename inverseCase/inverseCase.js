f09fad83a926510854a3621b0c1a9ad74cf85efd
Tolu Adesina <tadesina90@gmail.com>
2019-05-07 13:14:08 +0100

    Made declarations one per line and reduced the declaration where needed

--- inverseCase/inverseCase.js
+++ inverseCase/inverseCase.js
@@ -2,9 +2,9 @@ var toUpper = require('../toUpper/toUpper');
 var toLower = require('../toLower/toLower');
 
 String.prototype.inverseCase = function() {
-  var newString = '', regex = /[a-z]/;
+  var newString = ''; 
   for (var i = 0; i < this.length; i++) {
-    regex.test(this[i]) ? newString += this[i].toUpper() : newString += this[i].toLower();
+    (/[a-z]/).test(this[i]) ? newString += this[i].toUpper() : newString += this[i].toLower();
   }
   return newString;
 }
