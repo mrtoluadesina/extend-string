1f253db4c297aa65da94483707d3b2daa033dc41
Tolu Adesina <tadesina90@gmail.com>
2019-05-07 14:57:20 +0100

    Uninversal test for all methods - toUpper and hasVowels method tested

--- index.spec.js
+++ index.spec.js
@@ -1,5 +1,16 @@
-var index = require('./index');
+require('./index');
 
-test('Check to see that a string has at least one vowel and returns true, else returns false', function() {
-  expect()
-});
+describe('hasVowel Method Test', function() {
+  it('Check to see that a string has at least one vowel and returns true, else returns false', function() {
+    var oneVowel = 'tolu';
+    expect(oneVowel.hasVowels()).toBeTruthy();
+  });
+})
+
+describe('toUpper Method Test', function() {
+  it('Check to know if the string is converted properly to capital letters', function() {
+    var thisiS = 'today is not a weekend', capitalize = 'WEEKENDS ARE THE BEST';
+    expect(thisiS.toUpper()).toMatch(/[A-Z\s]{13}/); // using regex as the matcher for the function
+    expect(capitalize.toUpper()).toMatch('WEEKENDS ')
+  });
+})
