78165449d71a5d1c95b9490f01293ae9aab3f7f9
Tolu Adesina <tadesina90@gmail.com>
2019-05-07 13:19:00 +0100

    Added comment to explain what the ternary operator is doing

--- inverseCase/inverseCase.js
+++ inverseCase/inverseCase.js
@@ -4,6 +4,7 @@ var toLower = require('../toLower/toLower');
 String.prototype.inverseCase = function() {
   var newString = ''; 
   for (var i = 0; i < this.length; i++) {
+    // Here we test each iteration of the string and then if it is lowercase we make it uppercase and vice versa
     (/[a-z]/).test(this[i]) ? newString += this[i].toUpper() : newString += this[i].toLower();
   }
   return newString;
