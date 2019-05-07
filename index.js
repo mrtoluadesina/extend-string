009a00cc485883332389b7c4a2d1c2a61bd0fc40
Tolu Adesina <tadesina90@gmail.com>
2019-05-07 14:10:35 +0100

    Imported some methods to the main index file and exported two to make sure my process is working

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ var hasVowels = require('./hasVowels/hasVowels');
 var toUpper = require('./toUpper');
 var toLower = require('./toLower');
 var ucFirst = require('./ucFirst');
+var isQuestion = require('./isQuestion');
+var words = require('./words');
+var wordsCount = require('./wordsCount');
+var toCurrency = require('./toCurrency');
+var fromCurrency = require('./fromCurrency');
+var inverseCase = require('./inverseCase');
+
+
+module.exports = {hasVowels, toUpper};
