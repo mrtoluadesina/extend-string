df84f3ba79361d20baf41559483a8a57f4359a4b
Tolu Adesina <tadesina90@gmail.com>
2019-05-07 15:09:46 +0100

    toCurrency Method tested and passes test in the universal test file

--- index.spec.js
+++ index.spec.js
@@ -53,3 +53,10 @@ describe('wordsCount Method Test', function() {
   });
 });
 
+describe('toCurrency Method Test', function() {
+  it('returns a currency representation of the inputed string', function() {
+    var money = '12569400', notMoney = 'threeten';
+    expect(money.toCurrency()).toBe('12,569,400.00');
+    expect(notMoney.toCurrency()).toBe('NaN');
+  });
+})
